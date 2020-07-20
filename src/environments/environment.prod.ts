908ad7a076f1140569dda2a80817b7f60b4df9d9
Luuk Volkering <luuk.volkering@gmail.com>
2020-07-20 19:07:54 +0200

    Added environment variable to production.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+    apiKey: "AIzaSyAVDoMBzu-5ihx6KXMfl8q0-85fJLTC8aU",
+    authDomain: "luuk180-dev.firebaseapp.com",
+    databaseURL: "https://luuk180-dev.firebaseio.com",
+    projectId: "luuk180-dev",
+    storageBucket: "luuk180-dev.appspot.com",
+    messagingSenderId: "120505390023",
+    appId: "1:120505390023:web:95b2f3ffcac56239b0ffe2",
+    measurementId: "G-5PBYGQTL90"
+  }
 };
