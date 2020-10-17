12c51c24dd1a0eac05c9b8e566692169e527331f
Luuk Volkering <luuk.volkering@gmail.com>
2020-10-17 15:49:24 +0200

    Got the projects page working without firestore.

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,26 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 
-
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
+  entries;
 
-  constructor(db: AngularFirestore) {
-    var docRef = db.collection('GitHubAPI').doc('EcLVxMbaEJQXhChJwNUw');
-    const data = docRef.get().toPromise().then(function(doc) {
-      if (doc.exists) {
-          console.log("Document data:", doc.data().data);
-      } else {
-          // doc.data() will be undefined in this case
-          console.log("No such document!");
-      }
-  }).catch(function(error) {
-      console.log("Error getting document:", error);
-  });}
+  constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    getGitQuery().then((query) => this.entries = query);
+    console.log(this.entries);
+  }
+}
+
+async function getGitQuery(){
+  const response = await fetch('https://api.github.com/graphql', {
+        method: 'POST',
+        headers: {
+        'Authorization': `bearer 23daaaceb9b25674c3592ef17bfc58828d8d2b0a`
+        },
+        body: JSON.stringify({
+          query: `
+          {
+            user(login: "luuk180") {
+              repositories(first: 99, orderBy: {field: PUSHED_AT, direction: DESC}) {
+                nodes {
+                  name
+                  description
+                  url
+                  pushedAt
+                }
+              }
+            }
+          }
+          `,
+        }),
+  });
+  const json = await response.json();
+  const data = json.data.user.repositories.nodes;
+
+  return data;
 }
